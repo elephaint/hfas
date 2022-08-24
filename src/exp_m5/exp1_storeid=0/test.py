c540b7e9ae8aa973d83750651090cb43317f08bf
Olivier Sprangers <osprangers@gmail.com>
2022-08-24 15:31:22 +0200

    updatetest

--- src/exp_m5/exp1_storeid=0/test.py
+++ src/exp_m5/exp1_storeid=0/test.py
@@ -1,11 +1,7 @@
 #%%
-import os
-print(os.getcwd())
-import sys,os
-sys.path.append(os.getcwd())
+# Add this repository to path
 import pandas as pd
 import numpy as np
 from hierts.reconciliation import calc_summing_matrix, apply_reconciliation_methods, calc_level_method_rmse
 from src.exp_m5.helper_functions import read_m5, create_forecast_set 
 from src.exp_m5.experiments import exp_m5_globalall, exp_m5_sepagg, exp_m5_globalbottomup
-
