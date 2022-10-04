9f45cb09e1054aa1de3b2215a812224ebe5fe2a0
Olivier Sprangers <osprangers@gmail.com>
2022-10-04 10:37:25 +0200

    storeidexperiment

---
+++ src/exp_m5/exp1_storeid=0/load_params.py
@@ -0,0 +1,5 @@
+#%%
+import joblib
+params = joblib.load('src/exp_m5/exp1_storeid=0/bu_objtweedie_evalhmse_best_params.params')
+#%%
+params_new = joblib.load('src/exp_m5/exp1_storeid=0/bu_objhse_evalhmse_best_params.params')
